cfa67d3265c95642caec8db52e841240d0dd3984
Tina Huang <t42huang@gmail.com>
2020-03-13 09:45:26 -0400

    enable shooting bullets

--- settings.py
+++ settings.py
@@ -7,5 +7,14 @@ class Settings:
         self.screen_width = 800
         self.screen_height = 600
         self.bg_color = (230, 230, 230) # light grey
-        self.ship_speed = 20
         self.full_screen_mode = False
+
+        # voyager ship setting
+        self.ship_speed = 20
+
+        # bullet setting
+        self.bullet_speed = 30
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        
--- voyager.py
+++ voyager.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class Voyager:
     """Overall class to manage game assets and behavior."""
@@ -18,12 +19,14 @@ class Voyager:
         pygame.display.set_caption("Voyager")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _check_events(self):
@@ -36,7 +39,7 @@ class Voyager:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
-    
+            
     def _check_keydown_events(self, event):
         """Respond to keydown events"""
         if event.key == pygame.K_RIGHT:
@@ -48,6 +51,8 @@ class Voyager:
         elif event.key == pygame.K_f:
             self.settings.full_screen_mode = self.settings.full_screen_mode == False
             self._set_up_screen()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
     
     def _set_up_screen(self):
         """Set up the game canvas screen, full screen or not depending on the setting"""
@@ -65,7 +70,11 @@ class Voyager:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
-                
+
+    def _fire_bullet(self):
+        """Create a new bullet and add to the bullets group"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)            
         
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
@@ -73,6 +82,9 @@ class Voyager:
         self.screen.fill(self.settings.bg_color)
 
         self.ship.blitme()
+
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         
         # Make the most recently drawn screen visible.
         pygame.display.flip()
