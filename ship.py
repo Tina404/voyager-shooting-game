32c2b31f569a67153b800c136874eac143bfdad7
Tina Huang <t42huang@gmail.com>
2020-03-12 23:25:02 -0400

    enable continueous movement on left/right arrowkey press-hold

--- ship.py
+++ ship.py
@@ -11,6 +11,10 @@ class Ship:
         self.image = pygame.image.load('images/voyager.png') # 330 x 240 
         self.image = pygame.transform.scale(self.image, (33, 24))
         self.rect = self.image.get_rect()
+        
+        # Movement flag added to enable contineous movement
+        self.moving_right = False
+        self.moving_left = False
 
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
@@ -18,3 +22,10 @@ class Ship:
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
