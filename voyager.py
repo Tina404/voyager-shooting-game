1730decd9de39814a70f7f352ded947c79fd7a95
Tina Huang <t42huang@gmail.com>
2020-03-13 01:35:56 -0400

    enable full screen mode by pressing 'f'

--- voyager.py
+++ voyager.py
@@ -46,6 +46,10 @@ class Voyager:
             self.ship.moving_left = True
         elif event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
             sys.exit()
+        elif event.key == pygame.K_f:
+            self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+            self.settings.screen_width = self.screen.get_rect().width
+            self.settings.screen_height = self.screen.get_rect().height
     
     def _check_keyup_events(self, event):
         """Respond to keyup events"""
