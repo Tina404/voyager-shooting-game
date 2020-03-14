5c47bde2d5fe5b42f061a02dccb403f1f7be9692
Tina Huang <t42huang@gmail.com>
2020-03-14 07:29:03 -0400

    display game level

--- voyager.py
+++ voyager.py
@@ -84,6 +84,10 @@ class Voyager:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+        
+            # Increase level
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _check_events(self):
         """Respond to keypresses and mouse events"""
@@ -113,6 +117,7 @@ class Voyager:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             # get rid of any remaining aliens and bullets
             self.aliens.empty()
