85cbd6ef821a740cf3cbaf12b1b60ce06a6b519d
Tina Huang <t42huang@gmail.com>
2020-03-13 23:50:00 -0400

    check collission between ship and aliens

--- voyager.py
+++ voyager.py
@@ -160,6 +160,9 @@ class Voyager:
         self._check_fleet_edges()
         self.aliens.update()
 
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            print("Ship hit!!!")
+    
     def _check_fleet_edges(self):
         """Respond appropriately if any aliens have reached an edge."""
         for alien in self.aliens.sprites():
