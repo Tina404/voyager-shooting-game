0d590e65e9de2d732620f9da2b40c7f4b804afd6
Tina Huang <t42huang@gmail.com>
2020-03-14 06:57:49 -0400

    increase alien points as game level up

--- settings.py
+++ settings.py
@@ -25,11 +25,12 @@ class Settings:
         self.alien_speed = 5 # 10.0 # 1.0
         self.fleet_direction = 1 # 1 for moving right, -1 for moving left
 
-        # how quickly the game speeds up
+        # how quickly the game speeds up with game level
         self.speedup_scale = 1.1
 
         # scoring
         self.alien_points = 50
+        self.score_scale = 1.5 # score points increase with level
         
     def initialize_dynamic_settings(self):
         """Initialize settings that change throughout the game."""
@@ -40,7 +41,10 @@ class Settings:
         self.fleet_direction = 1
     
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and alien point values."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        print(self.alien_points)
